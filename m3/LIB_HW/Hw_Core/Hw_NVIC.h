56547380c2ab6cbc710ec90052420c3cb3d46af6
duckheewon <fain9301@yahoo.com>
2017-12-08 17:52:17 +0900

    fix nvic

--- m3/LIB_HW/Hw_Core/Hw_NVIC.h
+++ m3/LIB_HW/Hw_Core/Hw_NVIC.h
@@ -6,6 +6,8 @@
 #define NVIC_VectTab_RAM             ((uint32_t)0x20000000)
 #define NVIC_VectTab_FLASH           ((uint32_t)0x08000000)
 
+#define __NVIC_PRIO_BITS              4
+
 #ifdef HW_NVIC_LOCAL
 #define HW_NVIC_DEF
 #else
@@ -16,8 +18,7 @@
 HW_NVIC_DEF void NVIC_PriorityGroupConfig(uint32_t NVIC_PriorityGroup);
 HW_NVIC_DEF void NVIC_SetVectorTable(uint32_t NVIC_VectTab, uint32_t Offset);
 HW_NVIC_DEF void NVIC_Configuration(void);
-HW_NVIC_DEF void NVIC_SetPriority(IRQn_Type IRQn, uint32_t priority);
-HW_NVIC_DEF void NVIC_PriorityGroupConfig(uint32_t NVIC_PriorityGroup);
+HW_NVIC_DEF void NVIC_SetPriority(IRQn_TypeDef IRQn, uint32_t priority);
 HW_NVIC_DEF void NVIC_Init(NVIC_InitTypeDef* NVIC_InitStruct);
 
 
