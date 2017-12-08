bebc3ccdfa7c0ef8362772c31cb7dd627734091b
duckheewon <fain9301@yahoo.com>
2017-12-08 10:50:16 +0900

    rcc set

--- m3/LIB_HW/Hw_Core/Hw_Rcc.c
+++ m3/LIB_HW/Hw_Core/Hw_Rcc.c
@@ -12,21 +12,71 @@ RCC_DEF void HSI_Init(void)
     RCC->CR |= ((uint32_t)RCC_CR_HSION);
     do
     {
-        HSIStatus = RCC-CR & RCC_CR_HSIRDY;
+        HSIStatus = RCC->CR & RCC_CR_HSIRDY;
         StartUpCounter++;
     }while((HSIStatus == 0) && (StartUpCounter == 0x500));
     //flash access delay 24Mhz < SYSCLK <= 48Mhz
     FLASH->ACR |= FLASH_ACR_LATENCY_1;
     //PLL setting
     /* HCLK = SYSTICK */
-    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
-     /* PCLK2 = HCLK */
-    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
-     /* PCLK1 = HCLK */
-    RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+    RCC->CFGR |= (uint32_t)SYSCLK_NOT_DIVIDED;
+    /* PCLK2 = HCLK */
+    RCC->CFGR |= (uint32_t)APB2_PCLK2_NOT_DIVIDED;
+    /* PCLK1 = HCLK */
+    RCC->CFGR |= (uint32_t)APB1_PCLK1_DIVIDED2;
+    /* ADC = set div 4 */
+    RCC->CFGR |= (uint32_t)ADCPRE_PLCK2_DIVIDED4;
+    /* PLL ON */
+    RCC->CR |= RCC_CR_PLLON;
+    /* Wait till PLL is ready */
+    while((RCC->CR & RCC_CR_PLLRDY) == 0)
+    {
+        ;
+    }
+    /* select PLL as system clock source */
+    RCC->CFGR |= (uint32_t)SW_PLL_SYSCLOCK;
+    /* Wailt til PLL is used as system clock source */
+    while((RCC->CFGR & (uint32_t)SWS_MASK) != ((uint32_t)0x08))
+    {
+        ; //조건을 만족하면 여기 pll 준비가 안되어 있다는 것
+    }
 }
+
 
 RCC_DEF void HSE_Init(void)
 {
-
+    __IO uint32_t HSEStatus = 0;
+    RCC->CR |= ((uint32_t)RCC_CR_HSEON);
+    do
+    {
+        HSEStatus = RCC->CR & RCC_CR_HSERDY;
+        StartUpCounter++;
+    }while((HSEStatus == 0) && (StartUpCounter == 0x500));
+    //flash access delay 48MHz < SYSCLOCK <=72MHz
+    FLASH->ACR |= FLASH_ACR_LATENCY_2;
+     //PLL setting
+    /* HCLK = SYSTICK */
+    RCC->CFGR |= (uint32_t)SYSCLK_NOT_DIVIDED;
+    /* PCLK2 = HCLK */
+    RCC->CFGR |= (uint32_t)APB2_PCLK2_NOT_DIVIDED;
+    /* PCLK1 = HCLK */
+    RCC->CFGR |= (uint32_t)APB1_PCLK1_DIVIDED2;
+    /* ADC = set div 6 */
+    RCC->CFGR |= (uint32_t)ADCPRE_PLCK2_DIVIDED6;
+    /* PLL configuration : PLLCLK = HSE * 6 = 72MHz */
+    RCC->CFGR |= (uint32_t)(PLLSRC_HSE | PLL_INPUT_MULT6);
+    /* PLL ON */
+    RCC->CR |= RCC_CR_PLLON;
+    /* Wait till PLL is ready */
+    while((RCC->CR & RCC_CR_PLLRDY) == 0)
+    {
+        ;
+    }
+    /* select PLL as system clock source */
+    RCC->CFGR |= (uint32_t)SW_PLL_SYSCLOCK;
+    /* Wailt til PLL is used as system clock source */
+    while((RCC->CFGR & (uint32_t)SWS_MASK) != ((uint32_t)0x08))
+    {
+        ; //조건을 만족하면 여기 pll 준비가 안되어 있다는 것
+    }
 }
--- m3/LIB_HW/Hw_Core/Hw_Rcc.h
+++ m3/LIB_HW/Hw_Core/Hw_Rcc.h
@@ -15,23 +15,64 @@
 #define  RCC_CR_PLLON                        ((uint32_t)0x01000000)        /*!< PLL enable */
 #define  RCC_CR_PLLRDY                       ((uint32_t)0x02000000)        /*!< PLL clock ready flag */
 
-/** @defgroup AHB_clock_source 
-  * @{
-  */
+//System clock switch
+#define SW_HSI_SYSCLOCK                      ((uint32_t)0x00000000)
+#define SW_HSE_SYSCLOCK                      ((uint32_t)0x00000001)
+#define SW_PLL_SYSCLOCK                      ((uint32_t)0x00000002)
+#define SW_NOT_ALLOWED                       ((uint32_t)0x00000003)
+//System clock mask
+#define SWS_MASK                             ((uint32_t)0x0000000C)
+//ADC PREscaler
+#define ADCPRE_PLCK2_DIVIDED2                ((uint32_t)0x00000000)
+#define ADCPRE_PLCK2_DIVIDED4                ((uint32_t)0x00004000)
+#define ADCPRE_PLCK2_DIVIDED6                ((uint32_t)0x00006000)
+#define ADCPRE_PLCK2_DIVIDED8                ((uint32_t)0x0000C000)
+//APB2 PREscaler
+#define APB2_PCLK2_NOT_DIVIDED               ((uint32_t)0x00000000)
+#define APB2_PCLK2_DIVIDED2                  ((uint32_t)0x00002000)
+#define APB2_PCLK2_DIVIDED4                  ((uint32_t)0x00002800)
+#define APB2_PCLK2_DIVIDED8                  ((uint32_t)0x00003000)
+#define APB2_PCLK2_DIVIDED16                 ((uint32_t)0x00003800)
+//APB1 PREscaler
+#define APB1_PCLK1_NOT_DIVIDED               ((uint32_t)0x00000000)
+#define APB1_PCLK1_DIVIDED2                  ((uint32_t)0x00000400)
+#define APB1_PCLK1_DIVIDED4                  ((uint32_t)0x00000500)
+#define APB1_PCLK1_DIVIDED8                  ((uint32_t)0x00000600)
+#define APB1_PCLK1_DIVIDED16                 ((uint32_t)0x00000700)
+//AHB PREsacler
+#define SYSCLK_NOT_DIVIDED                   ((uint32_t)0x00000000)
+#define SYSCLK_DIVIDED2                      ((uint32_t)0x00000080)
+#define SYSCLK_DIVIDED4                      ((uint32_t)0x00000090)
+#define SYSCLK_DIVIDED8                      ((uint32_t)0x000000A0)
+#define SYSCLK_DIVIDED16                     ((uint32_t)0x000000B0)
+#define SYSCLK_DIVIDED64                     ((uint32_t)0x000000C0)
+#define SYSCLK_DIVIDED128                    ((uint32_t)0x000000D0)
+#define SYSCLK_DIVIDED256                    ((uint32_t)0x000000E0)
+#define SYSCLK_DIVIDED512                    ((uint32_t)0x000000F0)
+//pll use hse or hsi
+#define PLLSRC_HSI                           ((uint32_t)0x00000000)
+#define PLLSRC_HSE                           ((uint32_t)0x00010000)        /*!< HSE clock selected as PLL entry clock source */
+//pll mult
+#define PLL_INPUT_MULT2                      ((uint32_t)0x00000000)
+#define PLL_INPUT_MULT3                      ((uint32_t)0x00040000)
+#define PLL_INPUT_MULT4                      ((uint32_t)0x00080000)
+#define PLL_INPUT_MULT5                      ((uint32_t)0x000C0000)
+#define PLL_INPUT_MULT6                      ((uint32_t)0x00100000)
+#define PLL_INPUT_MULT7                      ((uint32_t)0x00140000)
+#define PLL_INPUT_MULT8                      ((uint32_t)0x00180000)
+#define PLL_INPUT_MULT9                      ((uint32_t)0x001C0000)
+#define PLL_INPUT_MULT10                     ((uint32_t)0x00200000)
+#define PLL_INPUT_MULT11                     ((uint32_t)0x00240000)
+#define PLL_INPUT_MULT12                     ((uint32_t)0x00280000)
+#define PLL_INPUT_MULT13                     ((uint32_t)0x002C0000)
+#define PLL_INPUT_MULT14                     ((uint32_t)0x00300000)
+#define PLL_INPUT_MULT15                     ((uint32_t)0x00340000)
+#define PLL_INPUT_MULT16                     ((uint32_t)0x00380000)
+#define PLL_INPUT_MULT2_16                     ((uint32_t)0x003C0000)
 
-#define RCC_SYSCLK_Div1                  ((uint32_t)0x00000000)
-#define RCC_SYSCLK_Div2                  ((uint32_t)0x00000080)
-#define RCC_SYSCLK_Div4                  ((uint32_t)0x00000090)
-#define RCC_SYSCLK_Div8                  ((uint32_t)0x000000A0)
-#define RCC_SYSCLK_Div16                 ((uint32_t)0x000000B0)
-#define RCC_SYSCLK_Div64                 ((uint32_t)0x000000C0)
-#define RCC_SYSCLK_Div128                ((uint32_t)0x000000D0)
-#define RCC_SYSCLK_Div256                ((uint32_t)0x000000E0)
-#define RCC_SYSCLK_Div512                ((uint32_t)0x000000F0)
 
-#define  RCC_CFGR_HPRE_DIV1                  ((uint32_t)0x00000000)        /*!< SYSCLK not divided */
-#define  RCC_CFGR_PPRE2_DIV1                 ((uint32_t)0x00000000)        /*!< HCLK not divided */
-#define  RCC_CFGR_PPRE1_DIV2                 ((uint32_t)0x00000400)        /*!< HCLK divided by 2 */
+
+
 
 
 #ifdef RCC_LOCAL
