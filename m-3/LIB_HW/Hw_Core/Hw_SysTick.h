1fe14f6abe18324db9e52de73fcd530ed13be7da
duckheewon <fain9301@yahoo.com>
2017-12-11 17:06:36 +0900

    nvic and exti finsh

--- m-3/LIB_HW/Hw_Core/Hw_SysTick.h
+++ m-3/LIB_HW/Hw_Core/Hw_SysTick.h
@@ -3,6 +3,10 @@
 
 #include "Hw.h"
 
+#define SYSTICK_ENABLE              0                                          /* Config-Bit to start or stop the SysTick Timer                         */
+#define SYSTICK_TICKINT             1                                          /* Config-Bit to enable or disable the SysTick interrupt                 */
+#define SYSTICK_CLKSOURCE           2                                          /* Clocksource has the offset 2 in SysTick Control and Status Register   */
+#define SYSTICK_MAXCOUNT       ((1<<24) -1)                                    /* SysTick MaxCount                                                      */
 
 #ifdef HW_SYSTICK_LOCAL
 #define HW_SYSTICK_DEF
@@ -11,7 +15,9 @@
 #endif
 
 
-HW_SYSTICK_DEF void 
+HW_SYSTICK_DEF uint32_t SysTick_Config(uint32_t ticks);
+HW_SYSTICK_DEF void SysTick_Handler(void);
+
 
 
 #endif
