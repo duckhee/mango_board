b7b84b70f2553ab3218918756972951942d4b27e
duckheewon <fain9301@yahoo.com>
2017-12-11 17:13:06 +0900

    command menu add

---
+++ m-3/MENU/Menu_Command.h
@@ -0,0 +1,15 @@
+#ifndef __MENU_COMMAND_H__
+#define __MENU_COMMAND_H__
+
+#include "Menu.h"
+
+
+#ifdef MENU_COMMAND_LOCAL
+#define MENU_COMMAND_DEF
+#else
+#define MENU_COMMAND_DEF                    extern
+#endif
+
+MENU_COMMAND_DEF void command_main(void);
+
+#endif
