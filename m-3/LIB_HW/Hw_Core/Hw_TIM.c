2535499e411f3aaea0b9a098feaa76dd1931d4a7
duckheewon <fain9301@yahoo.com>
2017-12-12 11:11:44 +0900

    tim add

--- m-3/LIB_HW/Hw_Core/Hw_TIM.c
+++ m-3/LIB_HW/Hw_Core/Hw_TIM.c
@@ -7,3 +7,68 @@ HW_TIM_DEF void TIM2_IRQHandler(void);
 HW_TIM_DEF void TIM_TimeBaseInit(TIM_TypeDef* TIMx, TIM_TimeBaseInitTypeDef* TIM_TimeBaseInitStruct);
 HW_TIM_DEF void TIM_Start_Cmd(TIM_TypeDef* TIMx, FunctionalState NewState);
 HW_TIM_DEF void TIM_Configuration(void);
+HW_TIM_DEF void TIM_PrescalerConfig(TIM_TypeDef* TIMx, uint16_t prescaler, uint16_t TIM_PscReloadMode);
+HW_TIM_DEF void TIM_ClearFlag(TIM_TypeDef* TIMx, uint16_t TIM_FLAG);
+
+
+HW_TIM_DEF void TIM_Configuration(void)
+{
+
+}
+
+HW_TIM_DEF void TIM2_IRQHandler(void)
+{
+
+}
+
+HW_TIM_DEF void TIM_ClearITPendingBit(TIM_TypeDef* TIMx, uint16_t TIM_IT)
+{
+    /* Clear the IT pending Bit */
+    TIMx->SR = (uint16_t)~TIM_IT;
+}
+
+HW_TIM_DEF void TIM_TimeBaseInit(TIM_TypeDef* TIMx, TIM_TimeBaseInitTypeDef* TIM_TimeBaseInitStruct)
+{
+    TIMx->CR1 &= TIM_CR1_CKD_Mask & TIM_CR1_CounterMode_Mask;
+    TIMx->CR1 |= (uint32_t)TIM_TimeBaseInitStruct->TIM_ClockDivision | TIM_TimeBaseInitStruct->TIM_CounterMode;
+    /* Set the Autoreload value */
+    TIMx->ARR = TIM_TimeBaseInitStruct->TIM_Period;
+    /* Set the Prescaler value */
+    TIMx->PSC = TIM_TimeBaseInitStruct->TIM_Prescaler;
+    if ((((uint32_t) TIMx) == TIM1_BASE) || (((uint32_t) TIMx) == TIM8_BASE))
+    {
+        /* Set the Repetition Counter value */
+        TIMx->RCR = TIM_TimeBaseInitStruct->TIM_RepetitionCounter;
+    }
+    /* Generate an update event to reload the Prescaler value immediatly */
+    TIMx->EGR = TIM_PSCReloadMode_Immediate;          
+
+}
+
+HW_TIM_DEF void TIM_Start_Cmd(TIM_TypeDef* TIMx, FunctionalState NewState)
+{
+    if (NewState != DISABLE)
+    {
+        /* Enable the TIM Counter */
+        TIMx->CR1 |= TIM_CR1_CEN_Set;
+    }
+    else
+    {
+        /* Disable the TIM Counter */
+        TIMx->CR1 &= TIM_CR1_CEN_Reset;
+    }
+}
+
+HW_TIM_DEF void TIM_PrescalerConfig(TIM_TypeDef* TIMx, uint16_t prescaler, uint16_t TIM_PscReloadMode)
+{
+    /* Set the Prescaler value */
+    TIMx->PSC = prescaler;
+    /* Set or reset the UG Bit */
+    TIMx->EGR = TIM_PscReloadMode;
+}
+
+HW_TIM_DEF void TIM_ClearFlag(TIM_TypeDef* TIMx, uint16_t TIM_FLAG)
+{
+    /* Clear the flags */
+    TIMx->SR = (uint16_t)~TIM_FLAG;
+}
--- m-3/LIB_HW/Hw_Core/Hw_TIM.h
+++ m-3/LIB_HW/Hw_Core/Hw_TIM.h
@@ -3,6 +3,12 @@
 
 #include "Hw.h"
 
+/* TIM FLAG */
+#define TIM_FLAG_Update                    ((uint16_t)0x0001)
+/* TIM RELOAD MODE */
+#define TIM_PSCReloadMode_Update           ((uint16_t)0x0000)
+#define TIM_PSCReloadMode_Immediate        ((uint16_t)0x0001)
+
 #define TIM_IT_Update                      ((uint16_t)0x0001)
 #define TIM_IT_CC1                         ((uint16_t)0x0002)
 #define TIM_IT_CC2                         ((uint16_t)0x0004)
@@ -11,6 +17,13 @@
 #define TIM_IT_COM                         ((uint16_t)0x0020)
 #define TIM_IT_Trigger                     ((uint16_t)0x0040)
 #define TIM_IT_Break                       ((uint16_t)0x0080)
+#define TIM_PSCReloadMode_Update           ((uint16_t)0x0000)
+#define TIM_PSCReloadMode_Immediate        ((uint16_t)0x0001)
+/* TIM register bit mask */
+#define TIM_CR1_CEN_Set                    ((uint16_t)0x0001)
+#define TIM_CR1_CEN_Reset                  ((uint16_t)0x03FE)
+#define TIM_CR1_CKD_Mask                   ((uint16_t)0x00FF)
+#define TIM_CR1_CounterMode_Mask           ((uint16_t)0x038F)
 
 #ifdef HW_TIM_LOCAL
 #define HW_TIM_DEF
@@ -23,6 +36,8 @@ HW_TIM_DEF void TIM2_IRQHandler(void);
 HW_TIM_DEF void TIM_TimeBaseInit(TIM_TypeDef* TIMx, TIM_TimeBaseInitTypeDef* TIM_TimeBaseInitStruct);
 HW_TIM_DEF void TIM_Start_Cmd(TIM_TypeDef* TIMx, FunctionalState NewState);
 HW_TIM_DEF void TIM_Configuration(void);
+HW_TIM_DEF void TIM_PrescalerConfig(TIM_TypeDef* TIMx, uint16_t prescaler, uint16_t TIM_PscReloadMode);
+HW_TIM_DEF void TIM_ClearFlag(TIM_TypeDef* TIMx, uint16_t TIM_FLAG);
 
 
 #endif
